6cef22cdf30fd63a09f16f5f5f573161ba1f5b69
Chetan Badhe <csbadhe08@gmail.com>
2019-02-26 23:42:44 +0530

    add matic function getMappedTokenAddress

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="transfer"> getMappedTokenAddress </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Matic from 'maticjs';
+import config from './config'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+    data() {
+      return {
+      }
+    },
+    methods: {
+        transfer() {
+        let tokenAddress = "0xc4375b7de8af5a38a93548eb8453a498222c4ff2";
+
+        const from = '0x7C27C62376f85797288b4Cc2B5b924d2FE70a462' // from address
+        const to = '0xc15d8950f6a98dc1708200b1ad368c95d3990ecb' // to address
+
+        const token = '0xdda2b0cea46316192fb440611acc1b36dd16f49f' // test token address
+        const newAmount = '4000000000000000000' // amount in wei
+        
+        console.log(config.MATIC_PROVIDER)
+        // Create object of Matic
+        const matic = new Matic({
+          maticProvider: config.MATIC_PROVIDER,
+          parentProvider: config.PARENT_PROVIDER,
+          rootChainAddress: config.ROOTCHAIN_ADDRESS,
+          syncerUrl: config.SYNCER_URL,
+          watcherUrl: config.WATCHER_URL,
+          maticWethAddress: config.MATICWETH_ADDRESS,
+        })
+
+        matic.wallet = '0x' + config.privateKey // prefix with `0x`
+
+      matic
+  .getMappedTokenAddress("0xC4375B7De8af5a38a93548eb8453a498222C4fF2")
+  .then(address => {
+    console.log("matic address", address)
+  })},
+    },
+    mounted() {
+
+    }
   }
-}
 </script>
 
 <style>
---
+++ src/config.js
@@ -0,0 +1,11 @@
+let config = {
+    MATIC_PROVIDER: 'https://testnet.matic.network',
+    PARENT_PROVIDER: 'https://kovan.infura.io/matic',
+    ROOTCHAIN_ADDRESS: '0x24e01716a6ac34D5f2C4C082F553D86a557543a7',
+    SYNCER_URL: 'https://eth-syncer.api.matic.network/api/v1',
+    WATCHER_URL: 'https://eth-watcher.api.matic.network/api/v1',
+    MATICWETH_ADDRESS: '0xFCbE295b918848001eB43941Db3fF9b6439a5e13',
+    privateKey:'',
+  }
+
+export default config;
